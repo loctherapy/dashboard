43de0d1e6785d7bea60ed6701582abd59c601732
Georg Panok <georg.panok@contentful.com>
2024-05-27 23:48:21 +0200

    TODO Printer with colors

--- internal/todo/todo_printer.go
+++ internal/todo/todo_printer.go
@@ -5,28 +5,51 @@ import (
 	"sort"
 	"strings"
 	"text/template"
+
+	"github.com/fatih/color"
 )
 
 type ToDoPrinter struct {
     Template *template.Template
 }
 
+type TemplateData struct {
+    ContextMap map[string][]FileToDos
+    PrintContext func(string) string
+    PrintFile func(string, int) string
+    PrintToDo func(string) string
+}
+
 func NewToDoPrinter() (*ToDoPrinter, error) {
-    const tmpl = `{{- range $context, $files := . }}
-Context: {{ $context }}
+    const tmpl = `{{- range $context, $files := .ContextMap }}
+{{ call $.PrintContext $context }}
 {{- range $files }}
-  File: {{ .FilePath }} (Gravity: {{ .Gravity }})
+  {{ call $.PrintFile .FilePath .Gravity }}
   {{- range .ToDos }}
-    {{ .Line }}
+    {{ call $.PrintToDo .Line }}
   {{- end }}
 {{ end }}
 {{ end }}`
-
     t, err := template.New("todo").Parse(tmpl)
     if err != nil {
         return nil, err
     }
     return &ToDoPrinter{Template: t}, nil
+}
+
+func printContext(context string) string {
+    contextColor := color.New(color.FgHiYellow, color.Bold).SprintFunc()
+    return contextColor(fmt.Sprintf("Context: %s\n", context))
+}
+
+func printFile(filePath string, gravity int) string {
+    fileColor := color.New(color.FgHiCyan).SprintFunc()
+    return fileColor(fmt.Sprintf("  File: %s (Gravity: %d)", filePath, gravity))
+}
+
+func printToDo(todo string) string {
+    todoColor := color.New(color.FgWhite).SprintFunc()
+    return todoColor(fmt.Sprintf("    %s", todo))
 }
 
 func (p *ToDoPrinter) Print(todos []FileToDos) error {
@@ -43,9 +66,17 @@ func (p *ToDoPrinter) Print(todos []FileToDos) error {
         })
     }
 
+    // Create template data
+    data := TemplateData{
+        ContextMap: contextMap,
+        PrintContext: printContext,
+        PrintFile: printFile,
+        PrintToDo: printToDo,
+    }
+
     // Render the template
     var sb strings.Builder
-    if err := p.Template.Execute(&sb, contextMap); err != nil {
+    if err := p.Template.Execute(&sb, data); err != nil {
         return err
     }
     fmt.Print(sb.String())
