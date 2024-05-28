fd05cca8b93ce052b02370f091844e6bd0edf404
Georg Panok <georg.panok@contentful.com>
2024-05-28 15:56:55 +0200

    Just buttons

--- internal/ui/ui_factory.go
+++ internal/ui/ui_factory.go
@@ -11,21 +11,24 @@ import (
 )
 
 type UIFactory struct {
-	app *tview.Application
-	header *tview.TextView
-	todoTextView *tview.TextView
-	flex *tview.Flex
+	app           *tview.Application
+	header        *tview.TextView
+	buttons       []*tview.Button
+	todoTextView  *tview.TextView
+	mainContainer *tview.Flex
 }
+
 
 func NewUIFactory() *UIFactory {
 	app := tview.NewApplication()
 	header := createMainHeader()
+	buttons := createButtons(app)
 	todoTextView := createTodoTextView(app)
-	flex := createLayout(header, todoTextView)
+	mainContainer := createLayout(header, buttons, todoTextView)
 
 	setupKeybindings(app)
 
-	return &UIFactory{app, header, todoTextView, flex}
+	return &UIFactory{app, header, buttons, todoTextView, mainContainer}
 }
 
 
@@ -42,6 +45,24 @@ func createMainHeader() *tview.TextView {
 		SetRegions(true)
 }
 
+func createButtons(app *tview.Application) []*tview.Button {
+    button1 := tview.NewButton("1 - All").SetSelectedFunc(func() {
+        // TODO: Add logic to filter and display all todos
+    })
+    button2 := tview.NewButton("2 - CF").SetSelectedFunc(func() {
+        // TODO: Add logic to filter and display CF todos
+    })
+    button3 := tview.NewButton("3 - GP").SetSelectedFunc(func() {
+        // TODO: Add logic to filter and display GP todos
+    })
+
+    buttons := make([]*tview.Button, 0)
+	buttons = append(buttons, button1, button2, button3)
+
+    return buttons
+}
+
+
 func createTodoTextView(app *tview.Application) *tview.TextView {
 	todoTextView := tview.NewTextView().
 		SetDynamicColors(true).
@@ -53,11 +74,19 @@ func createTodoTextView(app *tview.Application) *tview.TextView {
 	return todoTextView
 }
 
-func createLayout(header, todoTextView *tview.TextView) *tview.Flex {
-	return tview.NewFlex().
+func createLayout(header *tview.TextView, buttons []*tview.Button, todoTextView *tview.TextView) *tview.Flex {
+	flex := tview.NewFlex().
 		SetDirection(tview.FlexRow).
-		AddItem(header, 4, 1, false).
-		AddItem(todoTextView, 0, 1, true)
+		AddItem(header, 3, 1, false)
+
+	for _, button := range buttons {
+		flex.AddItem(button, 1, 1, false)
+	
+	}
+		
+	flex.AddItem(todoTextView, 0, 1, true)
+
+	return flex
 }
 
 func setupKeybindings(app *tview.Application) {
@@ -80,7 +109,7 @@ func startUpdatingTodos(app *tview.Application, todoTextView *tview.TextView) {
 			todoTextView.SetText(todosString)
 		})
 	}
-
+ 
 	// Initial fetch and update
 	go updateTodos()
 
@@ -121,5 +150,5 @@ func runUI(app *tview.Application, flex *tview.Flex) {
 func (f *UIFactory) Run() {
 	startUpdatingTodos(f.app, f.todoTextView)
 
-	runUI(f.app, f.flex)
+	runUI(f.app, f.mainContainer)
 }
