36f7cc60f2e6ce411eaaf17cd78f888a0f0eb95e
Georg Panok <georg.panok@contentful.com>
2024-05-29 10:21:26 +0200

    View - create context buttons dynamically

--- internal/view/todo_view.go
+++ internal/view/todo_view.go
@@ -3,6 +3,8 @@ package view
 import (
 	"fmt"
 	"os"
+	"sort"
+	"strings"
 
 	"github.com/gdamore/tcell/v2"
 	"github.com/loctherapy/dashboard/internal/model"
@@ -21,7 +23,7 @@ type View struct {
 func NewView(printer *ToDoPrinter) *View {
 	app := tview.NewApplication()
 	header := createMainHeader()
-	buttonFlex := createButtons(app)
+	buttonFlex := tview.NewFlex()
 	todoTextView := createTodoTextView(app)
 	mainContainer := createLayout(header, buttonFlex, todoTextView)
 
@@ -41,26 +43,6 @@ func createMainHeader() *tview.TextView {
 		SetTextColor(tcell.ColorGreen).
 		SetDynamicColors(true).
 		SetRegions(true)
-}
-
-func createButtons(app *tview.Application) *tview.Flex {
-	button1 := tview.NewButton("1 - All").SetSelectedFunc(func() {
-		// TODO: Add logic to filter and display all todos
-	})
-	button2 := tview.NewButton("2 - CF").SetSelectedFunc(func() {
-		// TODO: Add logic to filter and display CF todos
-	})
-	button3 := tview.NewButton("3 - GP").SetSelectedFunc(func() {
-		// TODO: Add logic to filter and display GP todos
-	})
-
-	buttonsFlex := tview.NewFlex().
-		SetDirection(tview.FlexColumn).
-		AddItem(button1, 0, 1, false).
-		AddItem(button2, 0, 1, false).
-		AddItem(button3, 0, 1, false)
-
-	return buttonsFlex
 }
 
 func createTodoTextView(app *tview.Application) *tview.TextView {
@@ -101,6 +83,39 @@ func (f *View) RunUI() {
 	}
 }
 
+func getContexts(todos []model.FileToDos) []string {
+	contexts := make(map[string]struct{})
+	for _, todo := range todos {
+		contexts[todo.Context] = struct{}{}
+	}
+
+	var contextList []string
+
+	// Add a button for all contexts
+	contextList = append(contextList, "All")
+
+	for context := range contexts {
+		contextList = append(contextList, context)
+	}
+
+	sort.Strings(contextList)
+
+	return contextList
+}
+
+func (f *View) createButtons(contexts []string) {
+	// Clear the buttonsFlex before adding new buttons
+	f.buttonsFlex.Clear()
+	
+	// Create a button for each context
+	for id, context := range contexts {
+		newId := id + 1
+		buttonName := fmt.Sprintf("%d - %s", newId, strings.ToUpper(context))
+		button := tview.NewButton(buttonName).SetSelectedFunc(func() {})
+		f.buttonsFlex.AddItem(button, 0, 1, false)
+	}
+}
+
 func (f *View) DisplayToDos(todos []model.FileToDos) {
 	// Use the printer to convert the todos to a string
 	todosString, err := f.Printer.Print(todos)
@@ -112,5 +127,11 @@ func (f *View) DisplayToDos(todos []model.FileToDos) {
 	// Queue the update to the todoTextView
 	f.app.QueueUpdateDraw(func() {
 		f.todoTextView.SetText(todosString)
+
+		// Get the unique contexts from the todos
+		contexts := getContexts(todos)
+		
+		// Create buttons for each context
+		f.createButtons(contexts)
 	})
 }
