6aaf4b0ae391530beba65cd09460fa52877ccaac
Georg Panok <georg.panok@contentful.com>
2024-05-29 20:54:18 +0200

    fetcher fmt

--- internal/repository/fetcher.go
+++ internal/repository/fetcher.go
@@ -8,40 +8,40 @@ import (
 )
 
 type FileFetcher struct {
-    FileRegex *regexp.Regexp
+	FileRegex *regexp.Regexp
 }
 
 func NewFileFetcher(pattern string) (*FileFetcher, error) {
-    if pattern == "" {
-        pattern = `.*\.md$`
-    }
+	if pattern == "" {
+		pattern = `.*\.md$`
+	}
 
-    re, err := regexp.Compile(pattern)
+	re, err := regexp.Compile(pattern)
 
-    if err != nil {
-        return nil, fmt.Errorf("invalid regex pattern: %v", err)
-    }
+	if err != nil {
+		return nil, fmt.Errorf("invalid regex pattern: %v", err)
+	}
 
-    return &FileFetcher{FileRegex: re}, nil
+	return &FileFetcher{FileRegex: re}, nil
 }
 
 func (f *FileFetcher) Fetch() ([]string, error) {
 
-    var files []string
+	var files []string
 
-    err := filepath.Walk(".", func(path string, info os.FileInfo, err error) error {
-        if err != nil {
-            return err
-        }
-        if !info.IsDir() && f.FileRegex.MatchString(info.Name()) {
-            files = append(files, path)
-        }
-        return nil
-    })
+	err := filepath.Walk(".", func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if !info.IsDir() && f.FileRegex.MatchString(info.Name()) {
+			files = append(files, path)
+		}
+		return nil
+	})
 
-    if err != nil {
-        return nil, err
-    }
+	if err != nil {
+		return nil, err
+	}
 
-    return files, nil
+	return files, nil
 }
