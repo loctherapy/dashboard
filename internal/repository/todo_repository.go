d91253f708cfa691a977e78f8b71c5c1e9f96da7
Georg Panok <georg.panok@contentful.com>
2025-01-29 21:37:13 +0100

    refactoring - making it less nested

--- internal/repository/todo_repository.go
+++ internal/repository/todo_repository.go
@@ -37,27 +37,33 @@ func (r *ToDoRepository) GetAll() ([]model.FileToDos, error) {
 	// Update cache with new or modified files
 	for _, file := range files {
 		cachedFile, exists := r.Cache.Get(file.Path)
-		if !exists || !file.ModTime.Equal(cachedFile.FileInfo.ModTime) {
-			context, contextGravity, gravity, err := r.FrontMatterParser.Parse(file.Path)
-			if err != nil {
-				return nil, err
-			}
-
-			todos, err := r.ToDoExtractor.Extract(file.Path)
-			if err != nil {
-				return nil, err
-			}
-
-			r.Cache.Push(file.Path, file.ModTime, []model.FileToDos{
-				{
-					FilePath:       file.Path,
-					ToDos:          todos,
-					Context:        context,
-					ContextGravity: contextGravity,
-					Gravity:        gravity,
-				},
-			})
+		if exists && file.ModTime.Equal(cachedFile.FileInfo.ModTime) {
+			continue
 		}
+
+		context, contextGravity, gravity, err := r.FrontMatterParser.Parse(file.Path)
+		if err != nil {
+			return nil, err
+		}
+
+		todos, err := r.ToDoExtractor.Extract(file.Path)
+		if err != nil {
+			return nil, err
+		}
+
+		if len(todos) == 0 {
+			continue
+		}
+
+		r.Cache.Push(file.Path, file.ModTime, []model.FileToDos{
+			{
+				FilePath:       file.Path,
+				ToDos:          todos,
+				Context:        context,
+				ContextGravity: contextGravity,
+				Gravity:        gravity,
+			},
+		})
 	}
 
 	// Remove files from cache that are no longer present
